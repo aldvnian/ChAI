84f93439dd81ad8b35093f260cc327adee5b8177
aldvnian <138926924+aldvnian@users.noreply.github.com>
2023-07-23 22:38:05 +0100

    Pawn.py
    Created pawn class with basic methods

--- Pawn.py
+++ Pawn.py
@@ -1,1 +1,17 @@
+class Pawn(Pieces):
+  def __init__(self):
+    super.()__init__(team, x, y):
 
+  def Move(self):
+    self.y = self.y + 1
+
+  def Kill_left(self):
+    self.y = self.y + 1
+    self.x = self.x - 1
+
+  def Kill_right(self):
+    self.y = self.y + 1
+    self.x = self.x + 1
+
+  def Promote(self):
+    

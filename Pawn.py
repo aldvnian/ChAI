414b377679f0f4afff924cb71935eca785d6eddb
aldvnian <138926924+aldvnian@users.noreply.github.com>
2023-07-24 18:56:01 +0100

    Update Pawn.py

--- Pawn.py
+++ Pawn.py
@@ -1,18 +1,18 @@
 from Piece import *
 
 class Pawn(Piece):
-  def __init__(self, team, x, y, boardRef):
-    super().__init__(team, x, y, boardRef)
+    def __init__(self, team, x, y, boardRef):
+        super().__init__(team, x, y, boardRef)
 
-  def Move(self):
-    self.y = self.y + 1
+    def Move(self):
+        self.y = self.y + 1
 
-  def Kill_left(self):
-    self.y = self.y + 1
-    self.x = self.x - 1
+    def Kill_left(self):
+        self.y = self.y + 1
+        self.x = self.x - 1
 
-  def Kill_right(self):
-    self.y = self.y + 1
-    self.x = self.x + 1
+    def Kill_right(self):
+        self.y = self.y + 1
+        self.x = self.x + 1
 
     

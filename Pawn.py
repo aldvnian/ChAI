204f9dc334f64a913a5ee3bf317866eeab462030
aldvnian <138926924+aldvnian@users.noreply.github.com>
2023-07-24 18:58:14 +0100

    Update Pawn.py

--- Pawn.py
+++ Pawn.py
@@ -1,8 +1,8 @@
 from Piece import *
 
 class Pawn(Piece):
-    def __init__(self, team, x, y, boardRef):
-        super().__init__(team, x, y, boardRef)
+    def __init__(self, team, x, y, boardReference):
+        super().__init__(team, x, y, boardReference)
 
     def Move(self):
         self.y = self.y + 1

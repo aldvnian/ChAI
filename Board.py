92c89769e845d933837e830c65b5e968a54001c1
aldvnian <138926924+aldvnian@users.noreply.github.com>
2023-07-24 18:26:24 +0100

    Update Board.py

--- Board.py
+++ Board.py
@@ -5,8 +5,8 @@ class Board:
             self.board.append([0] * 8)
         print(self.board)
         for x in range(8):
-            self.board[0][x] = Piece()
-            self.board[7][x] = Piece()
+            self.board[0][x] = Pawn()
+            self.board[7][x] = Pawn()
 
     def CheckPiece(self, x, y):
         return self.board[x][y]

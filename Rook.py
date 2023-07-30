a362e0fec9480e562b92ba5cec5fbe2776212cdc
AhmetKucukAK <ahmet.kucuk.ak@outlook.com>
2023-07-30 14:27:37 +0100

    Changed Rook to class.

--- Rook.py
+++ Rook.py
@@ -1,12 +1,6 @@
 from Piece import *
 
-def Rook(Piece):
-    def __init__(self, team, x, y, boardReference):
-        super().__init__(team, x, y, boardReference)
 
-    def move(self, x, y):
-        if self[x][y] == self.getValidMoves:
-            self.x = x
-            self.y = y
-
+class Rook(Piece):
+    pass
 

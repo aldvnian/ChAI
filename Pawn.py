7805e505c898c26ade11b629e041a8593a320f75
aldvnian <138926924+aldvnian@users.noreply.github.com>
2023-07-30 14:37:11 +0100

    Update Pawn.py

--- Pawn.py
+++ Pawn.py
@@ -1,12 +1,29 @@
 from Piece import *
 
-
 class Pawn(Piece):
     def __init__(self, team, x, y, boardReference):
         super().__init__(team, x, y, boardReference)
+        self.hasMoved = False
 
-    def move(self):
-        self.y = self.y + 1
+    def getValidMoves(self):
+        validMoves = []
+        
+        if self.board.checkPiece(self.getX(), self.getY() + team) == 0:
+            validMoves.append((self.getX(), self.getY() + team)
+            if not self.hasMoved and self.board.checkPiece(self.getX(), self.getY() + (2 * team)) == 0:
+                validMoves.append((self.getX(), self.getY() + (2 * team)))
 
-    def getValidMoves(self, board):
-        pass
+        if self.board.checkPiece(self.getX() + 1, self.getY() + team) == 0:
+            validMoves.append((self.getX() + 1, self.getY() + team))
+            
+        if self.board.checkPiece(self.getX() - 1, self.getY() + team) == 0:
+            validMoves.append((self.getX() - 1, self.getY() + team))
+    
+        
+        return validMoves
+
+    def display(self):
+        return "P"
+        
+    def firstMove(self):
+        self.hasMoved = True

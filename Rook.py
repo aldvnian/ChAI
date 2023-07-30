48da685986e9c82150092be4cc5db8d2f1a608a1
aldvnian <138926924+aldvnian@users.noreply.github.com>
2023-07-30 13:13:28 +0100

    Update Rook.py

--- Rook.py
+++ Rook.py
@@ -1,4 +1,4 @@
 from Piece import *
 def Rook(Piece):
-  def__init__(self, team, x, y, BoardReference):
-     super().__init__(team, x, y, BoardReference)
+  def__init__(self, team, x, y, boardReference):
+     super().__init__(team, x, y, boardReference)

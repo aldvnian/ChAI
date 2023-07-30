e23fb6cfb8a906682686d41892bc16ed37a04d65
aldvnian <138926924+aldvnian@users.noreply.github.com>
2023-07-30 12:44:11 +0100

    Update Pawn.py

--- Pawn.py
+++ Pawn.py
@@ -7,12 +7,4 @@ class Pawn(Piece):
     def Move(self):
         self.y = self.y + 1
 
-    def Kill_left(self):
-        self.y = self.y + 1
-        self.x = self.x - 1
-
-    def Kill_right(self):
-        self.y = self.y + 1
-        self.x = self.x + 1
-
     

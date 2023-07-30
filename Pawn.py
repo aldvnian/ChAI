78f1e53928dca5fc76f5a8bdf32d874950fe1975
aldvnian <138926924+aldvnian@users.noreply.github.com>
2023-07-30 13:02:45 +0100

    Update Pawn.py

--- Pawn.py
+++ Pawn.py
@@ -4,8 +4,8 @@ class Pawn(Piece):
     def __init__(self, team, x, y, boardReference):
         super().__init__(team, x, y, boardReference)
 
-    def Move(self):
+    def move(self):
         self.y = self.y + 1
 
-    def GetValidMoves(self):
+    def getValidMoves(self):
         pass

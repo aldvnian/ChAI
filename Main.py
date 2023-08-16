fc799495ad78fe665f561676903806c4fb57e4c6
AhmetKucukAK <ahmet.kucuk.ak@outlook.com>
2023-08-16 16:54:19 +0100

    Added code to ask users for input and move pieces.

--- Main.py
+++ Main.py
@@ -1,3 +1,23 @@
 from Board import *
+
 board = Board()
-board.display()
+
+runGame = True
+
+playerTurn = 1
+
+while runGame:
+    board.display()
+
+    pieceX = int(input("Enter your pieces x co-ordinate"))
+    pieceY = int(input("Enter your pieces y co-ordinate"))
+
+    moveX = int(input("Enter the x co-ordinate you wish to move to"))
+    moveY = int(input("Enter the y co-ordinate you wish to move to"))
+
+    board.movePiece(pieceX, pieceY, moveX, moveY)
+
+    if playerTurn == 1:
+        playerTurn = -1
+    else:
+        playerTurn = 1

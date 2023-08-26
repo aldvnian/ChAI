14b2f35f4bb7a12518f8f225d9f3332b631cd700
aldvnian <138926924+aldvnian@users.noreply.github.com>
2023-08-26 17:06:09 +0100

    Update Rook.py

--- Rook.py
+++ Rook.py
@@ -2,6 +2,9 @@ from Piece import *
 
 
 class Rook(Piece):
+    
     def display(self):
-        printreturn "R" def getValidMoves(self):
+        printreturn "R" 
+    
+    def getValidMoves(self):
         pass

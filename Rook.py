de77fcddd1a4c89610aa36eddf6492fadd565178
aldvnian <138926924+aldvnian@users.noreply.github.com>
2023-08-26 17:05:20 +0100

    Update Rook.py

--- Rook.py
+++ Rook.py
@@ -2,11 +2,6 @@ from Piece import *
 
 
 class Rook(Piece):
-    def __init__(self, x, y, boardReference):
-        super().__init__(x, y, boardReference)
-
     def display(self):
-        print("R")
-
-    def getValidMoves(self):
-        
+        printreturn "R" def getValidMoves(self):
+        pass

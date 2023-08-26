687a4d653bdb173415fe9a286ba802fc86f24d2b
aldvnian <138926924+aldvnian@users.noreply.github.com>
2023-08-26 17:33:44 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -13,10 +13,11 @@ while runGame:
         moveX = int(input("Enter the x co-ordinate you wish to move to"))
         moveY = int(input("Enter the y co-ordinate you wish to move to"))
         theSquare = board.checkPiece(pieceX, pieceY)
-        if (moveX, moveY) in theSquare.getValidMoves():
-        #move is valid
-            board.movePiece(pieceX, pieceY, moveX, moveY)
-            validMove = True
+        if theSquare != 0:
+            if (moveX, moveY) in theSquare.getValidMoves():
+                #move is valid
+                board.movePiece(pieceX, pieceY, moveX, moveY)
+                validMove = True
         else:
             print("Invalid move for that piece!")
         if playerTurn == 1:

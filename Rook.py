fc085246ee9138be21e1a07045434edd2e34c13c
aldvnian <138926924+aldvnian@users.noreply.github.com>
2023-08-26 16:42:06 +0100

    Update Rook.py

--- Rook.py
+++ Rook.py
@@ -3,7 +3,7 @@ from Piece import *
 
 class Rook(Piece):
     def __init__(self, x, y, boardReference):
-        super()
+        super().__init__(x, y, boardReference)
 
     def display(self):
         print("R")

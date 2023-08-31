8060b18a66d61284a12cb8004824d978ba3476b0
aldvnian <138926924+aldvnian@users.noreply.github.com>
2023-08-31 15:57:50 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,3 +1,4 @@
+#Imports from Board class
 from Board import *
 board = Board()
 runGame = True
@@ -6,14 +7,16 @@ playerTurn = 1
 while runGame:
     board.display()
     validMove = False
-    
+#Sets out the game to ask for inputs while validMove is True
+#Input: Coordinates of the piece to be moved and the coordinates where the piece is to be moved
     while not validMove:
         pieceX = int(input("Enter your pieces x co-ordinate"))
         pieceY = int(input("Enter your pieces y co-ordinate"))
 
         moveX = int(input("Enter the x co-ordinate you wish to move to"))
         moveY = int(input("Enter the y co-ordinate you wish to move to"))
-
+#Redundant?
+#Output: print statements
         theSquare = board.checkPiece(pieceX, pieceY)
         if theSquare != 0:
             if (moveX, moveY) in theSquare.getValidMoves():
@@ -24,7 +27,7 @@ while runGame:
                 print("Invalid move for that piece!")
         else:
             print("No piece at that location!")
-
+#Swaps player turns
         if playerTurn == 1:
             playerTurn = -1
         else:

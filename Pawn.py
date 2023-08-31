070dba72d7383e1ecd86e183a91467f679bd71db
aldvnian <138926924+aldvnian@users.noreply.github.com>
2023-08-31 15:44:54 +0100

    Update Pawn.py
    Descriptions of each subroutine

--- Pawn.py
+++ Pawn.py
@@ -1,10 +1,12 @@
 from Piece import *
-
+#Input: team 1,-1 - x, y, boardReference
+#initialising the constructor and setting a variable to record whether a pawn has moved
 class Pawn(Piece):
     def __init__(self, team, x, y, boardReference):
         super().__init__(team, x, y, boardReference)
         self.hasMoved = False
-
+#Output: all the valid moves for pawns
+#Stores valid moves for pawns
     def getValidMoves(self):
         validMoves = []
         
@@ -21,9 +23,9 @@ class Pawn(Piece):
     
         
         return validMoves
-
+#Output: displays "P" for pawn
     def display(self):
         return "P"
-        
+#Sets pawns to have moved
     def firstMove(self):
         self.hasMoved = True

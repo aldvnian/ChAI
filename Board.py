8815be05ace921d82cd56eb0bc9293710b8106f2
aldvnian <138926924+aldvnian@users.noreply.github.com>
2023-09-01 13:31:33 +0100

    Update Board.py

--- Board.py
+++ Board.py
@@ -1,34 +1,30 @@
-
 from Pawn import *
 
+
 class Board:
-    
-    @staticmethod
-    # Process:  ADD DESCRIPTION
-    def __init__(self):
-        self.board = []
+    board = []
 
-        for i in range(8):
-            self.board.append([0] * 8)
+    for i in range(8):
+        board.append([0] * 8)
 
-        for x in range(8):
-            self.board[0][x] = Pawn(1, x, 0, self)
-            self.board[7][x] = Pawn(-1, x, 7, self)
+    for x in range(8):
+        board[0][x] = Pawn(1, x, 0, self)
+        board[7][x] = Pawn(-1, x, 7, self)
 
-    #Inputs:    INPUT DESCRIPTION
+    # Inputs:    INPUT DESCRIPTION
     #           Integer, Integer
-    #Outputs:   Integer/Piece
-    #Purpose:   Takes in a set of coordinates and returns 0 if no piece is at that location, 
+    # Outputs:   Integer/Piece
+    # Purpose:   Takes in a set of coordinates and returns 0 if no piece is at that location,
     #           or a reference to the Piece object otherwise
     @staticmethod
     def checkPiece(self, x, y):
-        return self.board[y][x]
+        return Board.board[y][x]
 
-    #Outputs:   The boxes(_, |)
-    #Purpose:   It creates the outline of the chess board and displays the pieces on the board
+    # Outputs:   The boxes(_, |)
+    # Purpose:   It creates the outline of the chess board and displays the pieces on the board
     @staticmethod
     def display(self):
-        for row in self.board:
+        for row in Board.board:
             output = "|"
             for square in row:
                 if square == 0:
@@ -38,23 +34,22 @@ class Board:
                 output += "|"
             print(output)
 
-    #Inputs:    Coordinates of the piece to be moved/coordinates of where to move them 
+    # Inputs:    Coordinates of the piece to be moved/coordinates of where to move them
     #           integer, integer, integer, integer
-    #Outputs:   Final positions
-    #Purpose:   To check the piece in the given coordinates and move them to the specified coordinates
+    # Outputs:   Final positions
+    # Purpose:   To check the piece in the given coordinates and move them to the specified coordinates
     @staticmethod
     def movePiece(self, pieceX, pieceY, finalX, finalY):
 
-        piece = self.checkPiece(pieceX, pieceY)
+        piece = Board.checkPiece(pieceX, pieceY)
         if isinstance(piece, Pawn):
             piece.firstMove()
 
         piece.setX(finalX)
         piece.setY(finalY)
 
-        self.board[finalY][finalX] = piece
-        self.board[pieceY][pieceX] = 0
-
+        Board.board[finalY][finalX] = piece
+        Board.board[pieceY][pieceX] = 0
 
 
 

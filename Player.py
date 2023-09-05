e69167e8f0df9211327439567dcaf949ee178947
aldvnian <138926924+aldvnian@users.noreply.github.com>
2023-09-05 12:52:49 +0100

    Update Player.py

--- Player.py
+++ Player.py
@@ -3,5 +3,7 @@ from Piece import *
 class Player(Piece):
   def __init__(self, name):
     self.name = name
-
-  Piece.getx
+    self.x = int(input("Enter the X-Coordinate of the piece you want to move"))
+    self.y = int(input("Enter the Y-Coordinate of the piece you want to move"))
+    self.final_x = int(input("Enter the X-Coordinate of where you want to move it"))
+    self.final_y = int(input("Enter the Y-Coordinate of where you want to move it"))

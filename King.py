086c3b5f53083333af7c482cbfd43f321e8f1e04
aldvnian <138926924+aldvnian@users.noreply.github.com>
2023-09-05 13:45:10 +0100

    Update King.py

--- King.py
+++ King.py
@@ -3,3 +3,9 @@ from Piece import *
 class King:
     def __init__(self):
         super().__init__(team, x, y)
+
+    def display(self):
+        return "K"
+
+    def get_validmoves():
+        if self.checkpiece

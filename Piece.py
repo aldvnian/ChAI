658324035a6e3cd9e18aaba93588f4f83de27bb8
aldvnian <138926924+aldvnian@users.noreply.github.com>
2023-09-07 16:32:49 +0100

    Update Piece.py

--- Piece.py
+++ Piece.py
@@ -1,37 +1,41 @@
-class Piece:##I     Inputs:   - team -> integer
-     #           - x -> integer
-     #           - y -> integer
-     # Process:  Assigns variables when object is contructed def __init__(self, team, x, y):
+class Piece:
+    #Inputs:    - team -> integer
+    #           - x -> integer
+    #           - y -> integer
+    #Process:   - Assigns variables when a Piece object is constructed
+    def __init__(self, team, x, y):
         self.team = team
         self.x = x
         self.y = y
 
-#Method fo    r checking if a piece is in your team or opponent's
-#Input: oth    erPiece
+    #Method for checking if a piece is in your team or opponent's
+    #Input: otherPiece
     def checkSameTeam(self, otherPiece):
         return self.team == otherPiece.team
-#Gets the x 
-c        o    ordinate of a piece
+        
+    #Gets the x coordinate of a piece
     def getX(self):
         return self.x
-#Sets the x coo
-r        d    inate of a piece
+        
+    #Sets the x coordinate of a piece
     def setX(self, x):
         self.x = x
-#Gets the y coordi
-n        a    te of a piece
+        
+    #Gets the y coordinate of a piece
     def getY(self):
         return self.y
-#Sets the y coordinat
-e             of a piece
+        
+    #Sets the y coordinate of a piece
     def setY(self, y):
         self.y = y
-#Method for getting the 
-s        e    t of valid moves for a given piece
-#Input: board
-    def getVa    lidMoves(self):
+        
+    #Method for getting the set of valid moves for a given piece
+    #Input: board
+    def getValidMoves(self):
         pass
-#Method for displaying a pie
-c    e    
+    
+    #Method for displaying a piece
     def display(self):
         pass
+    
+

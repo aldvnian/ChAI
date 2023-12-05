f82e0773fbfb1abdc3e1a46814de38dbd89654d2
aldvnian <138926924+aldvnian@users.noreply.github.com>
2023-12-05 10:15:43 +0000

    Update Queen.py

--- Queen.py
+++ Queen.py
@@ -1,11 +1,12 @@
 from Player import *
 from Rook import *
+from Board import *
 
 class Queen(Piece):
     def __init__(self, x, y):
-        self.x = x
-        self.y = y
+        super.__init__(x, y)
         self.player = Player()
 
     def getValidMoves():
         ValidMoves = []
+        

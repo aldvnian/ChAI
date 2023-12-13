33374cd9b9cd07139b56f73977af0354e6d76255
aldvnian <138926924+aldvnian@users.noreply.github.com>
2023-12-13 11:46:29 +0000

    Update Queen.py

--- Queen.py
+++ Queen.py
@@ -7,7 +7,10 @@ class Queen(Piece):
         super.__init__(team, x, y)
 
     def maxX(self):
-        for x in range(self.x, 7)
+        for x in range(self.x, 8):
+            if self.x == isinstance(Piece):
+                return x-1
+            elif 
     
     def getValidMoves(self):
         ValidMoves = []

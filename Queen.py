2e2a4984d53225d223f27dbd42bc42c7f756608d
aldvnian <138926924+aldvnian@users.noreply.github.com>
2024-01-09 10:50:35 +0000

    Update Queen.py

--- Queen.py
+++ Queen.py
@@ -3,3 +3,15 @@ from Bishop import *
 from Piece import *
 
 class Queen():
+    def __init__(self, team, x, y):
+        super.__init__(team, x, y)
+
+    def display(self):
+        return "Q"
+
+    def getValidMoves(self):
+        validMoves = []
+        validMoves.append(self.Bishop.getValidMoves())
+        validMoves.append(self.Rook.getValidMoves())
+
+        return validMoves

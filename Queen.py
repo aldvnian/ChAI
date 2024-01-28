d014dc86a6cf038f65e0473fa9ee8171cf5fe8e0
Adnan <alvadn05@gmail.com>
2024-01-28 16:34:29 +0000

    Started on handling game rules

--- Queen.py
+++ Queen.py
@@ -1,16 +1,105 @@
 from Rook import *
-from Bishop import *
-from Piece import *
 from Board import *
 
 
-class Queen(Piece):
-    def __init__(self, team, x, y):
-        super().__init__(team, x, y)
-
+class Queen(Rook):
     def display(self):
         return "Q"
 
+    def leftDown(self):
+        x, y = self.getX(), self.getY()
+        # while (0 <= x <= 8) and (0 <= y <= 8):
+        for b in range(x, -1, -1):
+            if (b - 1 < 0) or (y - 1 < 0):
+                return b
+            currentSquare = Board.checkPiece(b - 1, y - 1)
+            if currentSquare != 0:
+                if not self.checkSameTeam(currentSquare):
+                    return b - 1
+                else:
+                    return b
+            y = y - 1
+        return 0
+
+    def rightDown(self):
+        x, y = self.getX(), self.getY()
+        # while (0 <= x <= 8) and (0 <= y <= 8):
+        for b in range(x, 8):
+            if (b + 1 > 7) or (y - 1 < 0):
+                return b
+            currentSquare = Board.checkPiece(b + 1, y - 1)
+            if currentSquare != 0:
+                if not self.checkSameTeam(currentSquare):
+                    return b + 1
+                else:
+                    return b
+            y = y - 1
+        return 7
+
+    def leftUp(self):
+        x, y = self.getX(), self.getY()
+        # while (0 <= x <= 8) and (0 <= y <= 8):
+        for a in range(x, -1, -1):
+            if (a - 1 < 0) or (y + 1 > 7):
+                return a
+            currentSquare = Board.checkPiece(a - 1, y + 1)
+            if currentSquare != 0:
+                if not self.checkSameTeam(currentSquare):
+                    return a - 1
+                else:
+                    return a
+            y = y + 1
+        return 0
+
+    def rightUp(self):
+        x, y = self.getX(), self.getY()
+        # while (0 <= x <= 8) and (0 <= y <= 8):
+        for a in range(x, 8):
+            if (a + 1 > 7) or (y + 1 > 7):
+                return a
+            currentSquare = Board.checkPiece(a + 1, y + 1)
+            if currentSquare != 0:
+                if not self.checkSameTeam(currentSquare):
+                    return a + 1
+                else:
+                    return a
+            y = y + 1
+        return 7
+
     def getValidMoves(self):
-        validMoves = [Bishop.getValidMoves(), Rook.getValidMoves()]
+        validMoves = super().getValidMoves() #rook valid moves added
+
+        x, y = self.getX(), self.getY()
+        leftDown = self.leftDown()
+        rightDown = self.rightDown()
+        leftUp = self.leftUp()
+        rightUp = self.rightUp()
+
+        # leftUp#
+
+        for a in range(x, leftUp, -1):
+            validMoves.append((a - 1, y + 1))
+            y += 1
+
+        x, y = self.getX(), self.getY()
+        # rightUp#
+
+        for b in range(x, rightUp):
+            validMoves.append((b + 1, y + 1))
+            y += 1
+
+        x, y = self.getX(), self.getY()
+        # leftDown#
+
+        for c in range(x, leftDown, -1):
+            validMoves.append((c - 1, y - 1))
+            y -= 1
+
+        x, y = self.getX(), self.getY()
+        # rightDown#
+
+        for d in range(x, rightDown):
+            validMoves.append((d + 1, y - 1))
+            y -= 1
+
         return validMoves

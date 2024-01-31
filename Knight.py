2d7894e7072a6fbd089a128de28e17e1cbbb55d5
Adnan <alvadn05@gmail.com>
2024-01-31 22:27:01 +0000

    Started on handling game rules

--- Knight.py
+++ Knight.py
@@ -8,15 +8,9 @@ class Knight(Piece):
 
     def TwoLeftUp(self):
         x, y = self.getX(), self.getY()
-<<<<<<< HEAD
         if (x - 2 >= 0) and (y + 1 <= 7):
             currentSquare = Board.checkPiece(x - 2, y + 1)
             if isinstance(currentSquare, Piece):
-=======
-        if (x - 2 >= 0) or (y + 1 <= 7):
-            currentSquare = Board.checkPiece(x - 2, y + 1)
-            if currentSquare != 0:
->>>>>>> origin/GameLogic
                 if not self.checkSameTeam(currentSquare):
                     return x - 2, y + 1
             else:
@@ -24,15 +18,9 @@ class Knight(Piece):
 
     def TwoLeftDown(self):
         x, y = self.getX(), self.getY()
-<<<<<<< HEAD
         if (x - 2 >= 0) and (y - 1 >= 0):
             currentSquare = Board.checkPiece(x - 2, y - 1)
             if isinstance(currentSquare, Piece):
-=======
-        if (x - 2 >= 0) or (y - 1 >= 0):
-            currentSquare = Board.checkPiece(x - 2, y - 1)
-            if currentSquare != 0:
->>>>>>> origin/GameLogic
                 if not self.checkSameTeam(currentSquare):
                     return x - 2, y - 1
             else:
@@ -40,15 +28,9 @@ class Knight(Piece):
 
     def TwoRightUp(self):
         x, y = self.getX(), self.getY()
-<<<<<<< HEAD
         if (x + 2 <= 7) and (y + 1 <= 7):
             currentSquare = Board.checkPiece(x + 2, y + 1)
             if isinstance(currentSquare, Piece):
-=======
-        if (x + 2 <= 7) or (y + 1 <= 7):
-            currentSquare = Board.checkPiece(x + 2, y + 1)
-            if currentSquare != 0:
->>>>>>> origin/GameLogic
                 if not self.checkSameTeam(currentSquare):
                     return x + 2, y + 1
             else:
@@ -56,15 +38,9 @@ class Knight(Piece):
 
     def TwoRightDown(self):
         x, y = self.getX(), self.getY()
-<<<<<<< HEAD
         if (x + 2 <= 7) and (y - 1 >= 0):
             currentSquare = Board.checkPiece(x + 2, y - 1)
             if isinstance(currentSquare, Piece):
-=======
-        if (x + 2 <= 7) or (y - 1 >= 0):
-            currentSquare = Board.checkPiece(x + 2, y - 1)
-            if currentSquare != 0:
->>>>>>> origin/GameLogic
                 if not self.checkSameTeam(currentSquare):
                     return x + 2, y - 1
             else:
@@ -72,15 +48,9 @@ class Knight(Piece):
 
     def OneLeftUp(self):
         x, y = self.getX(), self.getY()
-<<<<<<< HEAD
         if (y + 2 <= 7) and (x - 1 >= 0):
             currentSquare = Board.checkPiece(x - 1, y + 2)
             if isinstance(currentSquare, Piece):
-=======
-        if (y + 2 <= 7) or (x - 1 >= 0):
-            currentSquare = Board.checkPiece(x - 1, y + 2)
-            if currentSquare != 0:
->>>>>>> origin/GameLogic
                 if not self.checkSameTeam(currentSquare):
                     return x - 1, y + 2
             else:
@@ -88,15 +58,9 @@ class Knight(Piece):
 
     def OneLeftDown(self):
         x, y = self.getX(), self.getY()
-<<<<<<< HEAD
         if (y - 2 >= 0) and (x - 1 >= 0):
             currentSquare = Board.checkPiece(x - 1, y - 2)
             if isinstance(currentSquare, Piece):
-=======
-        if (y - 2 >= 0) or (x - 1 >= 0):
-            currentSquare = Board.checkPiece(x - 1, y - 2)
-            if currentSquare != 0:
->>>>>>> origin/GameLogic
                 if not self.checkSameTeam(currentSquare):
                     return x - 1, y - 2
             else:
@@ -104,15 +68,9 @@ class Knight(Piece):
 
     def OneRightUp(self):
         x, y = self.getX(), self.getY()
-<<<<<<< HEAD
         if (y + 2 <= 7) and (x + 1 <= 7):
             currentSquare = Board.checkPiece(x + 1, y + 2)
             if isinstance(currentSquare, Piece):
-=======
-        if (y + 2 <= 7) or (x + 1 <= 7):
-            currentSquare = Board.checkPiece(x + 1, y + 2)
-            if currentSquare != 0:
->>>>>>> origin/GameLogic
                 if not self.checkSameTeam(currentSquare):
                     return x + 1, y + 2
             else:
@@ -120,15 +78,9 @@ class Knight(Piece):
 
     def OneRightDown(self):
         x, y = self.getX(), self.getY()
-<<<<<<< HEAD
         if (y - 2 >= 0) and (x + 1 <= 7):
             currentSquare = Board.checkPiece(x + 1, y - 2)
             if isinstance(currentSquare, Piece):
-=======
-        if (y - 2 >= 0) or (x + 1 <= 7):
-            currentSquare = Board.checkPiece(x + 1, y - 2)
-            if currentSquare != 0:
->>>>>>> origin/GameLogic
                 if not self.checkSameTeam(currentSquare):
                     return x + 1, y - 2
             else:
@@ -138,8 +90,4 @@ class Knight(Piece):
         validMoves = [self.TwoLeftUp(), self.TwoLeftDown(), self.TwoRightUp(), self.TwoRightDown(), self.OneLeftUp(),
                       self.OneLeftDown(), self.OneRightUp(), self.OneRightDown()]
 
-<<<<<<< HEAD
-=======
-        print(validMoves)
->>>>>>> origin/GameLogic
         return validMoves

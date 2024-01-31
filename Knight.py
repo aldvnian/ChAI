25938f16cfa466a49e2596921f09b5894b1d5c1f
aldvnian <138926924+aldvnian@users.noreply.github.com>
2024-01-31 22:28:06 +0000

    Update Knight.py

--- Knight.py
+++ Knight.py
@@ -8,9 +8,9 @@ class Knight(Piece):
 
     def TwoLeftUp(self):
         x, y = self.getX(), self.getY()
-        if (x - 2 >= 0) or (y + 1 <= 7):
+        if (x - 2 >= 0) and (y + 1 <= 7):
             currentSquare = Board.checkPiece(x - 2, y + 1)
-            if currentSquare != 0:
+            if isinstance(currentSquare, Piece):
                 if not self.checkSameTeam(currentSquare):
                     return x - 2, y + 1
             else:
@@ -18,9 +18,9 @@ class Knight(Piece):
 
     def TwoLeftDown(self):
         x, y = self.getX(), self.getY()
-        if (x - 2 >= 0) or (y - 1 >= 0):
+        if (x - 2 >= 0) and (y - 1 >= 0):
             currentSquare = Board.checkPiece(x - 2, y - 1)
-            if currentSquare != 0:
+            if isinstance(currentSquare, Piece):
                 if not self.checkSameTeam(currentSquare):
                     return x - 2, y - 1
             else:
@@ -28,9 +28,9 @@ class Knight(Piece):
 
     def TwoRightUp(self):
         x, y = self.getX(), self.getY()
-        if (x + 2 <= 7) or (y + 1 <= 7):
+        if (x + 2 <= 7) and (y + 1 <= 7):
             currentSquare = Board.checkPiece(x + 2, y + 1)
-            if currentSquare != 0:
+            if isinstance(currentSquare, Piece):
                 if not self.checkSameTeam(currentSquare):
                     return x + 2, y + 1
             else:
@@ -38,9 +38,9 @@ class Knight(Piece):
 
     def TwoRightDown(self):
         x, y = self.getX(), self.getY()
-        if (x + 2 <= 7) or (y - 1 >= 0):
+        if (x + 2 <= 7) and (y - 1 >= 0):
             currentSquare = Board.checkPiece(x + 2, y - 1)
-            if currentSquare != 0:
+            if isinstance(currentSquare, Piece):
                 if not self.checkSameTeam(currentSquare):
                     return x + 2, y - 1
             else:
@@ -48,9 +48,9 @@ class Knight(Piece):
 
     def OneLeftUp(self):
         x, y = self.getX(), self.getY()
-        if (y + 2 <= 7) or (x - 1 >= 0):
+        if (y + 2 <= 7) and (x - 1 >= 0):
             currentSquare = Board.checkPiece(x - 1, y + 2)
-            if currentSquare != 0:
+            if isinstance(currentSquare, Piece):
                 if not self.checkSameTeam(currentSquare):
                     return x - 1, y + 2
             else:
@@ -58,9 +58,9 @@ class Knight(Piece):
 
     def OneLeftDown(self):
         x, y = self.getX(), self.getY()
-        if (y - 2 >= 0) or (x - 1 >= 0):
+        if (y - 2 >= 0) and (x - 1 >= 0):
             currentSquare = Board.checkPiece(x - 1, y - 2)
-            if currentSquare != 0:
+            if isinstance(currentSquare, Piece):
                 if not self.checkSameTeam(currentSquare):
                     return x - 1, y - 2
             else:
@@ -68,9 +68,9 @@ class Knight(Piece):
 
     def OneRightUp(self):
         x, y = self.getX(), self.getY()
-        if (y + 2 <= 7) or (x + 1 <= 7):
+        if (y + 2 <= 7) and (x + 1 <= 7):
             currentSquare = Board.checkPiece(x + 1, y + 2)
-            if currentSquare != 0:
+            if isinstance(currentSquare, Piece):
                 if not self.checkSameTeam(currentSquare):
                     return x + 1, y + 2
             else:
@@ -78,9 +78,9 @@ class Knight(Piece):
 
     def OneRightDown(self):
         x, y = self.getX(), self.getY()
-        if (y - 2 >= 0) or (x + 1 <= 7):
+        if (y - 2 >= 0) and (x + 1 <= 7):
             currentSquare = Board.checkPiece(x + 1, y - 2)
-            if currentSquare != 0:
+            if isinstance(currentSquare, Piece):
                 if not self.checkSameTeam(currentSquare):
                     return x + 1, y - 2
             else:
@@ -90,5 +90,4 @@ class Knight(Piece):
         validMoves = [self.TwoLeftUp(), self.TwoLeftDown(), self.TwoRightUp(), self.TwoRightDown(), self.OneLeftUp(),
                       self.OneLeftDown(), self.OneRightUp(), self.OneRightDown()]
 
-        print(validMoves)
         return validMoves

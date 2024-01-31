30b39a8a14d8f7b518423f43fee2bd5bf8d7cc0a
aldvnian <138926924+aldvnian@users.noreply.github.com>
2024-01-31 13:05:54 +0000

    Update Knight.py

--- Knight.py
+++ Knight.py
@@ -8,83 +8,83 @@ class Knight(Piece):
 
     def TwoLeftUp(self):
         x, y = self.getX(), self.getY()
-        if (x - 2 < 0) or (y + 1 > 7):
-            pass
-        else:
+        if (x - 2 >= 0) or (y + 1 <= 7):
             currentSquare = Board.checkPiece(x - 2, y + 1)
             if currentSquare != 0:
                 if not self.checkSameTeam(currentSquare):
                     return x - 2, y + 1
+            else:
+                return x - 2, y + 1
 
     def TwoLeftDown(self):
         x, y = self.getX(), self.getY()
-        if (x - 2 < 0) or (y - 1 < 0):
-            pass
-        else:
+        if (x - 2 >= 0) or (y - 1 >= 0):
             currentSquare = Board.checkPiece(x - 2, y - 1)
             if currentSquare != 0:
                 if not self.checkSameTeam(currentSquare):
                     return x - 2, y - 1
+            else:
+                return x - 2, y - 1
 
     def TwoRightUp(self):
         x, y = self.getX(), self.getY()
-        if (x + 2 > 7) or (y + 1 > 7):
-            pass
-        else:
+        if (x + 2 <= 7) or (y + 1 <= 7):
             currentSquare = Board.checkPiece(x + 2, y + 1)
             if currentSquare != 0:
                 if not self.checkSameTeam(currentSquare):
                     return x + 2, y + 1
+            else:
+                return x + 2, y + 1
 
     def TwoRightDown(self):
         x, y = self.getX(), self.getY()
-        if (x + 2 > 7) or (y - 1 < 0):
-            pass
-        else:
+        if (x + 2 <= 7) or (y - 1 >= 0):
             currentSquare = Board.checkPiece(x + 2, y - 1)
             if currentSquare != 0:
                 if not self.checkSameTeam(currentSquare):
                     return x + 2, y - 1
+            else:
+                return x + 2, y - 1
 
     def OneLeftUp(self):
         x, y = self.getX(), self.getY()
-        if (y + 2 > 7) or (x - 1 < 0):
-            pass
-        else:
+        if (y + 2 <= 7) or (x - 1 >= 0):
             currentSquare = Board.checkPiece(x - 1, y + 2)
             if currentSquare != 0:
                 if not self.checkSameTeam(currentSquare):
                     return x - 1, y + 2
+            else:
+                return x - 1, y + 2
 
     def OneLeftDown(self):
         x, y = self.getX(), self.getY()
-        if (y - 2 < 0) or (x - 1 < 0):
-            pass
-        else:
+        if (y - 2 >= 0) or (x - 1 >= 0):
             currentSquare = Board.checkPiece(x - 1, y - 2)
             if currentSquare != 0:
                 if not self.checkSameTeam(currentSquare):
                     return x - 1, y - 2
+            else:
+                return x - 1, y - 2
 
     def OneRightUp(self):
         x, y = self.getX(), self.getY()
-        if (y + 2 > 7) or (x + 1 > 7):
-            pass
-        else:
+        if (y + 2 <= 7) or (x + 1 <= 7):
             currentSquare = Board.checkPiece(x + 1, y + 2)
             if currentSquare != 0:
                 if not self.checkSameTeam(currentSquare):
                     return x + 1, y + 2
+            else:
+                return x + 1, y + 2
 
     def OneRightDown(self):
         x, y = self.getX(), self.getY()
-        if (y - 2 < 0) or (x + 1 > 7):
-            pass
-        else:
+        if (y - 2 >= 0) or (x + 1 <= 7):
             currentSquare = Board.checkPiece(x + 1, y - 2)
             if currentSquare != 0:
                 if not self.checkSameTeam(currentSquare):
                     return x + 1, y - 2
+            else:
+                return x + 1, y - 2
 
     def getValidMoves(self):
         validMoves = [self.TwoLeftUp(), self.TwoLeftDown(), self.TwoRightUp(), self.TwoRightDown(), self.OneLeftUp(),

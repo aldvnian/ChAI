1ed4081c8789a2ec27519d1b8b07d8f989987938
Adnan <alvadn05@gmail.com>
2024-02-08 22:24:58 +0000

    Started on handling game rules

--- UI.py
+++ UI.py
@@ -1,33 +1,52 @@
 import pygame
 
 pygame.init()
-WIDTH = 1000
-HEIGHT = 1000
-screen = pygame.display.set_mode([WIDTH, HEIGHT])
-screen.fill('light gray')
-font = pygame.font.Font('Font/syn-nova/SYNNova-Normal.otf', 20)
-bigFont = pygame.font.Font('Font/syn-nova/SYNNova-Normal.otf', 150)
+
 timer = pygame.time.Clock()
 fps = 60
-header1 = bigFont.render('Ch', True, (150, 150, 150))
-header2 = bigFont.render('AI', True, (255, 165, 0))
-header2Rect = header2.get_rect(midleft=(490, 200))
-header1Rect = header1.get_rect(midright=(490, 200))
-background = pygame.image.load('Images/background.jpg').convert_alpha()
-rect = pygame.Rect(315, 400, 310, 400)
 
-run = True
-while run:
-    timer.tick(fps)
-    mouse = pygame.mouse.get_pos()
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
 
-    screen.blit(background, (0, 0))
-    screen.blit(header1, header1Rect)
-    screen.blit(header2, header2Rect)
-    pygame.draw.rect(screen, (150, 150, 150), rect, 10)
+class mainMenu:
+    def __init__(self):
+        self.WIDTH = 1000
+        self.HEIGHT = 1000
+        self.screen = pygame.display.set_mode([self.WIDTH, self.HEIGHT])
+        self.screen.fill('light gray')
+        self.font = pygame.font.Font('Font/syn-nova/SYNNova-Normal.otf', 20)
+        self.bigFont = pygame.font.Font('Font/syn-nova/SYNNova-Normal.otf', 150)
+        self.header1 = self.bigFont.render('Ch', True, (150, 150, 150))
+        self.header2 = self.bigFont.render('AI', True, (255, 165, 0))
+        self.header2Rect = self.header2.get_rect(midleft=(490, 200))
+        self.header1Rect = self.header1.get_rect(midright=(490, 200))
+        self.background = pygame.image.load('Images/background.jpg').convert_alpha()
+        self.rect = pygame.Rect(315, 400, 310, 400)
 
-    pygame.display.flip()
-pygame.quit()
+    def run(self):
+        run = True
+        while run:
+            timer.tick(fps)
+            mouse = pygame.mouse.get_pos()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
+
+            self.screen.blit(self.background, (0, 0))
+            self.screen.blit(self.header1, self.header1Rect)
+            self.screen.blit(self.header2, self.header2Rect)
+            pygame.draw.rect(self.screen, (150, 150, 150), self.rect, 10)
+
+            pygame.display.flip()
+        pygame.quit()
+
+
+class button:
+    def __init__(self, image, x, y, width, height, font, textInput, baseColour, hoveringColour):
+        self.image = image
+        self.x, self.y = x, y
+        self.textInput = textInput
+        self.baseColour = baseColour
+        self.hoveringColour = hoveringColour
+        self.font = font
+        self.width = width
+        self.height = height
+        self.text = self.font.render(self.textInput, True, self.baseColour)

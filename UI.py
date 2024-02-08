a38b4364a64dcc32161699de28bdbf2727886edd
Adnan <alvadn05@gmail.com>
2024-02-08 21:57:27 +0000

    Started on handling game rules

--- UI.py
+++ UI.py
@@ -4,18 +4,30 @@ pygame.init()
 WIDTH = 1000
 HEIGHT = 1000
 screen = pygame.display.set_mode([WIDTH, HEIGHT])
-font = pygame.font.Font('freesandbold.ttf', 20)
-bigFont = pygame.font.Font('freesandbold.ttf', 45)
+screen.fill('light gray')
+font = pygame.font.Font('Font/syn-nova/SYNNova-Normal.otf', 20)
+bigFont = pygame.font.Font('Font/syn-nova/SYNNova-Normal.otf', 150)
 timer = pygame.time.Clock()
 fps = 60
+header1 = bigFont.render('Ch', True, (150, 150, 150))
+header2 = bigFont.render('AI', True, (255, 165, 0))
+header2Rect = header2.get_rect(midleft=(490, 200))
+header1Rect = header1.get_rect(midright=(490, 200))
+background = pygame.image.load('Images/background.jpg').convert_alpha()
+rect = pygame.Rect(315, 400, 310, 400)
 
 run = True
 while run:
     timer.tick(fps)
-    screen.fill('light gray')
+    mouse = pygame.mouse.get_pos()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
+    screen.blit(background, (0, 0))
+    screen.blit(header1, header1Rect)
+    screen.blit(header2, header2Rect)
+    pygame.draw.rect(screen, (150, 150, 150), rect, 10)
 
     pygame.display.flip()
 pygame.quit()

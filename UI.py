dbadad06023a7af565b7e710a2ec134c986aec68
Adnan <alvadn05@gmail.com>
2024-02-11 18:16:05 +0000

    Started on handling game rules

--- UI.py
+++ UI.py
@@ -1,9 +1,14 @@
 import pygame
+from Button import *
 
 pygame.init()
 
 timer = pygame.time.Clock()
 fps = 60
+font = pygame.font.Font('Font/syn-nova/SYNNova-Normal.otf', 40)
+bigFont = pygame.font.Font('Font/syn-nova/SYNNova-Normal.otf', 150)
+getButtonImage = pygame.image.load('Images/playButton.png')
+buttonImage = pygame.transform.scale(getButtonImage, (250, 75))
 
 
 class mainMenu:
@@ -11,15 +16,16 @@ class mainMenu:
         self.WIDTH = 1000
         self.HEIGHT = 1000
         self.screen = pygame.display.set_mode([self.WIDTH, self.HEIGHT])
+        pygame.display.set_caption('ChAI', 'ChAI')
         self.screen.fill('light gray')
-        self.font = pygame.font.Font('Font/syn-nova/SYNNova-Normal.otf', 20)
-        self.bigFont = pygame.font.Font('Font/syn-nova/SYNNova-Normal.otf', 150)
-        self.header1 = self.bigFont.render('Ch', True, (150, 150, 150))
-        self.header2 = self.bigFont.render('AI', True, (255, 165, 0))
+        self.header1 = bigFont.render('Ch', True, (150, 150, 150))
+        self.header2 = bigFont.render('AI', True, (255, 165, 0))
         self.header2Rect = self.header2.get_rect(midleft=(490, 200))
         self.header1Rect = self.header1.get_rect(midright=(490, 200))
+        self.playButton = Button(buttonImage, 475, 500, 'Play a match!', font, (255, 165, 0), 	(0,128,128))
+        self.settingsButton = Button(buttonImage, 475, 600, 'Settings', font, (255, 165, 0), (0,128,128))
+        self.tutorialButton = Button(buttonImage, 475, 700, 'Tutorial', font, (255, 165, 0), (0,128,128))
         self.background = pygame.image.load('Images/background.jpg').convert_alpha()
-        self.rect = pygame.Rect(315, 400, 310, 400)
 
     def run(self):
         run = True
@@ -33,21 +39,12 @@ class mainMenu:
             self.screen.blit(self.background, (0, 0))
             self.screen.blit(self.header1, self.header1Rect)
             self.screen.blit(self.header2, self.header2Rect)
-            pygame.draw.rect(self.screen, (150, 150, 150), self.rect, 10)
+            self.playButton.update(self.screen)
+            self.settingsButton.update(self.screen)
+            self.tutorialButton.update(self.screen)
 
             pygame.display.flip()
         pygame.quit()
 
 p1 = mainMenu()
 p1.run()
-class button:
-    def __init__(self, image, x, y, width, height, font, textInput, baseColour, hoveringColour):
-        self.image = image
-        self.x, self.y = x, y
-        self.textInput = textInput
-        self.baseColour = baseColour
-        self.hoveringColour = hoveringColour
-        self.font = font
-        self.width = width
-        self.height = height
-        self.text = self.font.render(self.textInput, True, self.baseColour)

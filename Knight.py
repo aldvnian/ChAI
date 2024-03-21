13dfa3a4ed97bb56deb93f737c8653bd8a4409e4
aldvnian <138926924+aldvnian@users.noreply.github.com>
2024-03-21 09:45:44 +0000

    Update Knight.py

--- Knight.py
+++ Knight.py
@@ -6,6 +6,7 @@ class Knight(Piece):
     def display(self):
         return 'K'
 
+    #Method for producing all the valid moves that are two squares to the left and one square up from the knight
     def TwoLeftUp(self):
         x, y = self.getX(), self.getY()
         if (x - 2 >= 0) and (y + 1 <= 7):
@@ -16,6 +17,7 @@ class Knight(Piece):
             else:
                 return x - 2, y + 1
 
+    #Method for producing all the valid moves that are two squares to the left and one square down from the knight
     def TwoLeftDown(self):
         x, y = self.getX(), self.getY()
         if (x - 2 >= 0) and (y - 1 >= 0):
@@ -26,6 +28,7 @@ class Knight(Piece):
             else:
                 return x - 2, y - 1
 
+    #Method for producing all the valid moves that are two squares to the right and one square up from the knight
     def TwoRightUp(self):
         x, y = self.getX(), self.getY()
         if (x + 2 <= 7) and (y + 1 <= 7):
@@ -36,6 +39,7 @@ class Knight(Piece):
             else:
                 return x + 2, y + 1
 
+    #Method for producing all the valid moves that are two squares to the right and one square down from the knight
     def TwoRightDown(self):
         x, y = self.getX(), self.getY()
         if (x + 2 <= 7) and (y - 1 >= 0):
@@ -46,6 +50,7 @@ class Knight(Piece):
             else:
                 return x + 2, y - 1
 
+    #Method for producing all the valid moves that are one square left and two squares up from the knight
     def OneLeftUp(self):
         x, y = self.getX(), self.getY()
         if (y + 2 <= 7) and (x - 1 >= 0):
@@ -56,6 +61,7 @@ class Knight(Piece):
             else:
                 return x - 1, y + 2
 
+    #Method for producing all the valid moves that are one square left and two squares down from the knight
     def OneLeftDown(self):
         x, y = self.getX(), self.getY()
         if (y - 2 >= 0) and (x - 1 >= 0):
@@ -66,6 +72,7 @@ class Knight(Piece):
             else:
                 return x - 1, y - 2
 
+    #Method for producing all the valid moves that are one square right and two squares up from the knight
     def OneRightUp(self):
         x, y = self.getX(), self.getY()
         if (y + 2 <= 7) and (x + 1 <= 7):
@@ -76,6 +83,7 @@ class Knight(Piece):
             else:
                 return x + 1, y + 2
 
+    #Method for producing all the valid moves that are one square right and two squares down from the knight
     def OneRightDown(self):
         x, y = self.getX(), self.getY()
         if (y - 2 >= 0) and (x + 1 <= 7):
@@ -86,6 +94,7 @@ class Knight(Piece):
             else:
                 return x + 1, y - 2
 
+    #Method for retrieving all of the valid moves for the knight
     def getValidMoves(self):
         validMoves = [self.TwoLeftUp(), self.TwoLeftDown(), self.TwoRightUp(), self.TwoRightDown(), self.OneLeftUp(),
                       self.OneLeftDown(), self.OneRightUp(), self.OneRightDown()]
